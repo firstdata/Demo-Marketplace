ef19a9bddd945d2872783191b182a3d916904bb9
Gregg Mojica <gwmojica@gmail.com>
2016-10-02 13:37:49 -0400

    add FAQ, reccommended, specs, features

--- public/products/products.js
+++ public/products/products.js
@@ -10,32 +10,54 @@ angular.module('products', ['ngRoute', 'ui.router'])
 }])
 
 .controller('productsCtrl', ['$routeParams', '$scope', '$http', function($routeParams, $scope, $http) {
-
 $scope.product = '';
 $scope.productIncludes = '';
 $scope.productFeatures = '';
+$scope.productFAQ = '';
+$scope.productSpecs = '';
+$scope.productRecommended = '';
 
   $http({
     method: 'GET',
     url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/details/'
   }).then(function successCallback(response) {
-    $scope.product = response;
+      $scope.product = response;
+  }, function errorCallback(response) { });
 
-    }, function errorCallback(response) {
-        //alert('An error occurred. Please refresh the page');
-    });
+  $http({
+    method: 'GET',
+    url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/includes/'
+  }).then(function successCallback(response) {
+      $scope.productIncludes = response;
+  }, function errorCallback(response) { });
 
+  $http({
+    method: 'GET',
+    url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/features/'
+  }).then(function successCallback(response) {
+    $scope.productFeatures = response;
+    }, function errorCallback(response) { });
 
+  $http({
+    method: 'GET',
+    url: 'https://dev.services.firstdata.com/v1/faq/' + $routeParams.id
+  }).then(function successCallback(response) {
+    $scope.productFAQ = response;
+    }, function errorCallback(response) { });
 
-      $http({
-        method: 'GET',
-        url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/features/'
-      }).then(function successCallback(response) {
-        $scope.productFeatures = response;
-        console.log($scope.productFeatures.data[0]);
-        }, function errorCallback(response) {
-            //alert('An error occurred. Please refresh the page');
-        });
+    $http({
+      method: 'GET',
+      url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/specs/'
+    }).then(function successCallback(response) {
+      $scope.productSpecs = response;
+      }, function errorCallback(response) { });
 
+    $http({
+      method: 'GET',
+      url: 'https://dev.services.firstdata.com/v1/products/' + $routeParams.id + '/recommended/'
+    }).then(function successCallback(response) {
+      console.log(response);
+      $scope.productRecommended = response;
+      }, function errorCallback(response) { });
 
 }]);
