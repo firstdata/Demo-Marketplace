97206c3d513edebf431a59a1406bce1bf9807917
bigsauron <vardan.bagiryan@gmail.com>
2016-11-14 11:25:34 -0800

    redirect to unsupported browsers if no session storage available

--- web/browsers.js
+++ web/browsers.js
@@ -8,31 +8,35 @@
   case 'IE':
     if (parseInt(ua.browser.version) < 9) {
       oldBrowser();
+      return;
     }
     break;
   case 'Firefox':
     if (parseInt(ua.browser.version) < 4) {
       oldBrowser();
+      return;
     }
     break;
   case 'Chrome':
     if (parseFloat(ua.browser.version) < 3.8) {
       oldBrowser();
+      return;
     }
     break;
   case 'Safari':
     if (parseInt(ua.browser.version) < 6) {
       oldBrowser();
-    } else {
-      var testKey = 'test', storage = window.localStorage;
-      try {
-        storage.setItem(testKey, '1');
-        storage.removeItem(testKey);
-      } catch (error) {
-        oldBrowser();
-      }
+      return;
     }
     break;
-  } 
+  }
 
+  var testKey = 'test', storage = window.localStorage;
+  try {
+    storage.setItem(testKey, '1');
+    storage.removeItem(testKey);
+  } catch (error) {
+    oldBrowser();
+    return;
+  }
 })(window);
